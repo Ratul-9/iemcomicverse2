c8eda361236b01a7c79d2624513424e762fa6f24
Ratul-9 <mukherjeeratul2023@gmail.com>
2025-06-07 00:14:55 +0530

    Made changes

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -44,7 +44,7 @@ const Navbar = () => {
                   scrollToSection(item.id);
                 }}
                 whileHover={{ scale: 1.1 }}
-                className={`relative group font-[Comic Neue] font-bold text-lg tracking-wide ${
+                className={`relative group font-akira font-bold text-lg tracking-wide ${
                   item.isButton
                     ? "px-5 py-2 rounded-lg bg-red-500 text-white border-2 border-black shadow-md hover:bg-red-600 transition-all cursor-pointer"
                     : "text-white font-akira text-sm hover:text-blue-600 transition-colors cursor-pointer"
